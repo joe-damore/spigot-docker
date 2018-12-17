55e1dbd343a820e55b2ad53b74f630364278740b
Joe D'Amore <joe@joedamore.me>
2018-12-17 18:35:37 -0500

    Replace spigot_install_status.sh with spigot_version.sh

---
+++ files/usr/local/spigot/spigot_version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Echo the current installed Spigot version, or "NONE" if not installed
+for f in "$SPIGOT_HOME/spigot-*";
+do
+    [ -e "$f" ] && echo "$f";
+    exit 0
+done
+
+exit 1
