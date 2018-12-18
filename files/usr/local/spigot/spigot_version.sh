15d52d8ec52cc42abaf9489ed7358f847b2a6cbe
Joe D'Amore <joe@joedamore.me>
2018-12-17 20:04:51 -0500

    Update spigot_version.sh

--- files/usr/local/spigot/spigot_version.sh
+++ files/usr/local/spigot/spigot_version.sh
@@ -2,7 +2,14 @@
 
 # Echo the current installed Spigot version, or "NONE" if not installed
 
-spigot_file=$(find "$SPIGOT_HOME"/spigot -maxdepth 1 -iname 'spigot-*' -print)
+# Make sure home directory exists
+if [ ! -d "$SPIGOT_HOME" ]
+then
+    mkdir "$SPIGOT_HOME"
+fi
+
+# Find spigot-*.jar in $SPIGOT_HOME
+spigot_file=$(find "$SPIGOT_HOME" -maxdepth 1 -iname 'spigot-*' -print)
 if [ -n "$spigot_file" ]
 then
     if [ $version =~ \\"$SPIGOT_HOME"\/spigot-(*.).jar ]
