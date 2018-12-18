cf7f6f505e38a5ff9c1b2dff0157ac816cc14054
Joe D'Amore <joe@joedamore.me>
2018-12-17 19:34:36 -0500

    Use find for spigot_version.sh

--- files/usr/local/spigot/spigot_version.sh
+++ files/usr/local/spigot/spigot_version.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
 # Echo the current installed Spigot version, or "NONE" if not installed
-echo "Spigot version"
 
-for f in "$SPIGOT_HOME"/spigot-*;
-do
-    echo "Found"
-    echo "$f"
-
-    if [ -e "$f" ]
-    then
-        echo "BINGO!"
-        echo "$f"
-    fi
+spigot_file=$(find "$SPIGOT_HOME"/spigot -maxdepth 1 -iname 'spigot-*' -print -quit)
+if [ -n "$spigot_file" ]
+then
+    echo "$spigot_file"
     exit 0
-done
+fi
 
 echo "NONE"
 exit 1
