733f25777242ad294f3a91d54e225fdc1f0cdeb1
Joe D'Amore <joe@joedamore.me>
2018-12-18 18:45:59 -0500

    Add start script to spigot_start.sh

--- files/usr/local/spigot/spigot_start.sh
+++ files/usr/local/spigot/spigot_start.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-echo "60"
+cd "$SPIGOT_HOME" || exit 1
+
+spigot_version=$(/bin/bash /usr/local/spigot/spigot_version.sh)
+
+if [ "$spigot_version" = "NONE" ]
+then
+    echo "Unable to start Spigot: Spigot is not installed."
+    exit 2
+fi
+
+spigot_jar="spigot-$spigot_version.jar"
+
+java -Xms1G -Xmx2G -XX:+UseConcMarkSweepGC -jar "$spigot_jar"
